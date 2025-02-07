52358d8de625e68a9e16f615b25080ec9f8690e9
Alessandro Rosario <121242080+4gora@users.noreply.github.com>
2025-02-07 19:15:23 -0300

    aula 102 completa

--- src/Excecoes95/runtime/test/RuntimeExceptionTest04.java
+++ src/Excecoes95/runtime/test/RuntimeExceptionTest04.java
@@ -5,6 +5,9 @@ package Excecoes95.runtime.test;
 /*Não podemos colocar um catch com uma exceção genérica na frente de uma mais específica,
 pois o código irá parar no primeiro catch que se encaixa, no caso, o mais genérico.*/
 
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+
 public class RuntimeExceptionTest04 {
     public static void main(String[] args) {
         try {
@@ -18,5 +21,29 @@ public class RuntimeExceptionTest04 {
         }  catch (ArithmeticException e) {
             System.out.println("Dentro do ArithmeticException");
         }
+
+// VIDEO 102 - multi catch em linha
+/*O caractere "|" permite que capture exceções de LINHA DE HERANÇA DIFERENTES com o mesmo catch*/
+
+        // mesmo código do video 101 com multi catch em linha
+        try {
+            throw new ArithmeticException();
+        } catch (ArrayIndexOutOfBoundsException | ArithmeticException | IllegalArgumentException e) {
+            System.out.println("Dentro do ArrayIndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Dentro do IndexOutOfBoundsException");
+
+        }
+
+
+        try {
+            talvezLanceException();
+        } catch (SQLException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void talvezLanceException() throws SQLException, FileNotFoundException {
+
     }
 }
