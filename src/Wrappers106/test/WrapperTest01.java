6cf01a596cbb7c51bf9345ad1d323ae5292c5355
Alessandro Rosario <121242080+4gora@users.noreply.github.com>
2025-02-08 13:01:15 -0300

    aula 106 completa

---
+++ src/Wrappers106/test/WrapperTest01.java
@@ -0,0 +1,34 @@
+package Wrappers106.test;
+// video 106 - wrappers pt 01
+public class WrapperTest01 {
+    public static void main(String[] args) {
+
+        byte byteP = 1;
+        short shortP = 1;
+        int inP = 1;
+        long longP = 1;
+        float floatP = 1;
+        double doubleP = 10D;
+        char charP = 'W';
+        boolean booleanP = false;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer inW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'W';
+        Boolean booleanW = false;
+
+        // classes wrapper transformam tipos primitivos em objetos;
+
+        int a = 5;
+        modificar(a);
+        System.out.println(a);
+
+    }
+    public static void modificar(int x) {
+        x = 10; // o valor da variável original não é alterada pelo método
+    }
+}
