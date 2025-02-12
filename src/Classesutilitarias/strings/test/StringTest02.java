9fdd6dfaf7cc848a727f746f6395e22a516a8d08
Alessandro Rosario <121242080+4gora@users.noreply.github.com>
2025-02-11 21:05:44 -0300

    aula 109 completa, strings 2

---
+++ src/Classesutilitarias/strings/test/StringTest02.java
@@ -0,0 +1,13 @@
+package Classesutilitarias.strings.test;
+
+public class StringTest02 {
+    // video 109 - Strings 2
+    public static void main(String[] args) {
+        String nome = "Luffy";
+        String numeros = "012345";
+        System.out.println(nome.charAt(1));
+        System.out.println(nome.length());
+        System.out.println(nome.replace("f", "l"));
+        System.out.println(numeros.substring(0,2));
+    }
+}
